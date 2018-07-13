dd6bd206d544bb2284e096be5fad4ed93338ab93
AgentMacklin <2015yZ250F>
2018-07-12 21:32:19 -0400

    cleaned up visual studio files

--- flightcode/eom.cpp
+++
@@ -1,8 +0,0 @@
-#include "eom.h"
-
-double getAccelZ(Adafruit_MMA8451 sensor) {
-	sensor.read();
-	sensors_event_t event;
-	sensor.getEvent(&event);
-	return event.acceleration.z * -meterToFeet;
-}
--- flightcode/flightcode.ino
+++ flightcode/flightcode.ino
@@ -23,7 +23,7 @@ Kalman altitudeFilter = Kalman(0.01);  // Or something, I don't know what the ac
 Kalman accelFilter = Kalman(0.05);     // Again, no idea what this is actually supposed to be.
 
 namespace eom {
-	double altitude;
+    double altitude;
 }
 
 // motor shield object
@@ -39,10 +39,10 @@ Adafruit_BME280 bme;
 
 // Runmode enum allows the arduino to shift between operation states in flight
 enum Runmode : unsigned char {
-	idle,		// Rocket is sitting on launchpad
-	launch,		// Motor is ignited and burning (have to wait for burnout)
-	coast,		// Motor is burnt out, this is where the fairings will do their thing
-	descent		// Rocket is now descending - retract fairings and power down
+    idle,		// Rocket is sitting on launchpad
+    launch,		// Motor is ignited and burning (have to wait for burnout)
+    coast,		// Motor is burnt out, this is where the fairings will do their thing
+    descent		// Rocket is now descending - retract fairings and power down
 };
 
 // We want to start off with idle on the launchpad.
@@ -52,68 +52,68 @@ Runmode MODE = idle;
 // the setup function runs once when you press reset or power the board
 void setup() {
 
-	// VARS
-	const int channelA = 3;
-	const int channelB = 4;
-	int encoderTicks = 0;                // ticks for encoder
+    // VARS
+    const int channelA = 3;
+    const int channelB = 4;
+    int encoderTicks = 0;                // ticks for encoder
 
 
-	// Initialize the sensor 
-	Serial.begin(9600);
-	// sets the modes of the channel pins to input
-	pinMode(channelA, INPUT);
-	pinMode(channelB, INPUT);
+    // Initialize the sensor 
+    Serial.begin(9600);
+    // sets the modes of the channel pins to input
+    pinMode(channelA, INPUT);
+    pinMode(channelB, INPUT);
 
-	// create with the default frequency 1.6KHz
-	AFMS.begin();
+    // create with the default frequency 1.6KHz
+    AFMS.begin();
 
-	// attach interrupt to pin 3 for encoder output
-	//	attachInterrupt(digitalPinToInterrupt(3), channelAEvent, CHANGE);
+    // attach interrupt to pin 3 for encoder output
+    //	attachInterrupt(digitalPinToInterrupt(3), channelAEvent, CHANGE);
 
 
-	// test pressure sensor 
-	if (!bme.begin()) {
-		Serial.println("Pressure sensor was not found. Check the wiring.");
-		while (1);
-	}
+    // test pressure sensor 
+    if (!bme.begin()) {
+        Serial.println("Pressure sensor was not found. Check the wiring.");
+        while (1);
+    }
 
-	// test acceleration sensor
-	if (!mma.begin()) {
-		Serial.println("Acceleration sensor was not found. Check the wiring.");
-		while (1);
-	}
-	mma.setRange(MMA8451_RANGE_2_G);
+    // test acceleration sensor
+    if (!mma.begin()) {
+        Serial.println("Acceleration sensor was not found. Check the wiring.");
+        while (1);
+    }
+    mma.setRange(MMA8451_RANGE_2_G);
 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
 
-	// Sitting on the launch pad.
-	if (MODE == idle) {
-		if (getAccelZ(mma) > 0) {
-			MODE = launch;
-		}
-	}
+    // Sitting on the launch pad.
+    if (MODE == idle) {
+        if (getAccelZ(mma) > 0) {
+            MODE = launch;
+        }
+    }
 
-	// Motor is burning, have to wait until burn out.
-	else if (MODE == launch) {
-		if (getAccelZ(mma) < 0) {
-			MODE = coast;
-		}
-	}
+    // Motor is burning, have to wait until burn out.
+    else if (MODE == launch) {
+        if (getAccelZ(mma) < 0) {
+            MODE = coast;
+        }
+    }
 
-	else if (MODE == coast) {
-		eom::altitude = altitudeFilter.output(bme.readAltitude(SEALEVELPRESSURE_HPA));
-		// Do all the controller stuff and data logging.
-		if (calculate_velocity(eom::altitude) < 0) {
-			MODE = descent;
-		}
-	}
+    else if (MODE == coast) {
+        eom::altitude = altitudeFilter.output(bme.readAltitude(SEALEVELPRESSURE_HPA));
+        // Do all the controller stuff and data logging.
+        if (calculate_velocity(eom::altitude) < 0) {
+            MODE = descent;
+        }
+    }
 
-	else if (MODE == descent) {
-		// Retract the fairings, close the data file, and power down after all that's done.
-	}
+    else if (MODE == descent) {
+        // Retract the fairings, close the data file, and power down after all that's done.
+    }
 
 
 }
